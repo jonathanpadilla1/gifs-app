cb7136aed13c821b7f78450949e07258555e0d1e
Jonathan Padilla <padilla.jon@hotmail.com>
2022-07-23 12:18:24 -0500

    new commit

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,22 +1,26 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
+import { GifsService } from '../services/gifs.service';
+
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styles: [],
 })
 export class BusquedaComponent {
-  constructor() {}
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  @ViewChild("txtBuscar") txtBuscar!:ElementRef<HTMLInputElement>;
+  constructor(private gifsService: GifsService) {}
 
   buscar() {
-    
-   const valor = this.txtBuscar.nativeElement.value;
-   console.log(valor);
+    const valor = this.txtBuscar.nativeElement.value;
 
-   this.txtBuscar.nativeElement.value = "";
+    if (valor.trim().length === 0) {
+      return;
+    }
 
+    this.gifsService.buscarGifs(valor);
+
+    this.txtBuscar.nativeElement.value = '';
   }
 }
-
